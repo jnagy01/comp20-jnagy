74e05efc6c5459558757ccba1ca035be4f50a250
jnagy01 <jordan.nagy@tufts.edu>
2016-03-07 21:09:45 -0500

    Updated javascript file

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,21 @@
 // Your JavaScript goes here...
+function parse() {
+	request = new XMLHttpRequest();
+
+	request.open("GET", "data.json", true);
+
+	request.onreadystatechange = function () {
+		if(request.readyState == 4){
+			result = "";
+			raw = request.responseText;
+			message = JSON.parse(raw);
+			elem = getElementById("messages");
+			for(i=0; i<message.length; i++){
+				result += "<p> message["i+1"]["content"] + " " message[i]["i+1"] </p>"
+			}
+			elem.innerHTML = result;
+		}
+	};
+
+	request.send(null); //no data to send
+}
